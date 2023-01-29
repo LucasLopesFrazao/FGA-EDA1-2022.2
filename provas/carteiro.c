25ed8791e9747ed90ea830f45e4a1b0b7a073e71
LucasLopesFrazao <211031771@aluno.unb.br>
2023-01-28 22:03:55 -0300

    Adicionando carteiro

---
+++ provas/carteiro.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int casas, pacotes;
+    scanf("%d %d", &casas, &pacotes);
+
+    int *casa = (int *)malloc(casas * sizeof(int));
+    int *pacote = (int *)malloc(pacotes * sizeof(int));
+
+    for (int i = 0; i < casas; i++) {
+        scanf("%d%*c", &casa[i]);
+    }
+    for (int i = 0; i < pacotes; i++) {
+        scanf("%d%*c", &pacote[i]);
+    }
+
+    int unidade = 0;
+    int z = 0;
+    for (int i = 0; i < pacotes; i++) {
+        while (pacote[i] != casa[z]) {
+            unidade++;
+            if (pacote[i] > casa[z]) {
+                z++;
+            } else {
+                z--;
+            }
+        }
+    }
+    printf("%d\n", unidade);
+
+    free(casa);
+    free(pacote);
+    return 0;
+}
+
